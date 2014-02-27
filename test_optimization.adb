c98ff4c6e58b5e2c5f67573fedb9d11ca6579663
charlesareynolds <charlesareynolds@gmail.com>
2014-02-26 21:49:51 -0800

    Adding different matrix component sizes.
    About 1.26E-9 per cell at 512**3

--- test_optimization.adb
+++ test_optimization.adb
@@ -1,47 +1,52 @@
 with Ada.Real_Time;
 with Ada.Text_IO;
-with GNAT.Source_Info;
 with Unchecked_Deallocation;
 
 procedure Test_Optimization is
    package ATI renames Ada.Text_IO;
    package ART renames Ada.Real_Time;
-   function Location return string
-     renames GNAT.Source_Info.Source_Location;
-   type Integer_Matrix is array
-     (Positive range <>, Positive range <>, Positive range <>) of Integer;
-   type Integer_Matrix_Access is access Integer_Matrix;
+   type Value_Type is new Short_Short_Integer;
+   type Value_Matrix is array
+     (Positive range <>,
+      Positive range <>,
+      Positive range <>) of Value_Type;
+   type Value_Matrix_Access is access Value_Matrix;
    procedure Free is new Unchecked_Deallocation
-     (Object => Integer_Matrix,
-      Name   => Integer_Matrix_Access);
+     (Object => Value_Matrix,
+      Name   => Value_Matrix_Access);
 
    procedure Test_Matrix (Size : in Positive) is
-      Integers : Integer_Matrix_Access;
-      Start_Time : ART.Time;
-      Elapsed : Duration;
+      Values          : Value_Matrix_Access;
+      Start_Time      : ART.Time;
+      Elapsed         : Duration;
+      Iterations      : Natural := 0;
       Time_Per_Capita : Float;
       use type ART.Time;
    begin
       ATI.Put_line ("Testing" & Size'Img & " cubed matrix.");
       Start_Time := ART.Clock;
-      Integers := new Integer_Matrix (1..Size, 1..Size, 1..Size);
-      for X in Integers'Range(1) loop
-         for Y in Integers'Range(2) loop
-            for Z in Integers'Range(3) loop
-               Integers (X,Y,Z) := X+Y+Z;
+      Values := new Value_Matrix (1..Size, 1..Size, 1..Size);
+      for repeats in 1..10 loop
+         for X in Values'Range(1) loop
+            for Y in Values'Range(2) loop
+               for Z in Values'Range(3) loop
+                  Iterations := Iterations + 1;
+                  Values (X,Y,Z) := Value_Type(55);
+               end loop;
             end loop;
          end loop;
       end loop;
       Elapsed := ART.To_Duration(ART.Clock - Start_Time);
+      ATI.Put_line ("Iterations:" & Iterations'Img);
       ATI.Put_line ("Elapsed time:" & Elapsed'Img);
-      Time_Per_Capita := Float(Elapsed) / Float (Size * Size * Size);
+      Time_Per_Capita := Float(Elapsed) / Float (Iterations);
       ATI.Put_line ("Time per element:" & Time_Per_Capita'Img);
-      Free (Integers);
+      Free (Values);
    end Test_Matrix;
 
 begin
    ATI.Put_line ("BEGIN");
-   for Index in 0..20 loop
+   for Index in 0..9 loop
       Test_Matrix (2**Index);
    end loop;
    ATI.Put_line ("END");
