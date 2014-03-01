d693974c167754951dd53bc7cbddf8eec08203a6
charlesareynolds <charlesareynolds@gmail.com>
2014-02-28 16:15:50 -0800

    Update lulesh-par.ads

--- lulesh-par.ads
+++ lulesh-par.ads
@@ -1,27 +1,10 @@
 package LULESH.Par is
---- // lulesh-par
 
    --x Real_t CalcElemVolume( const Real_t x[8],
    --x                        const Real_t y[8],
    --x                        const Real_t z[8]);
-   type Eight_Reals is array (0..7) of Real_t;
    function CalcElemVolume
-     (x : in Eight_Reals;
-      y : in Eight_Reals;
-      z : in Eight_Reals)
-     return Real_t;
-
-   --- // lulesh-par
-
-   --x Real_t CalcElemVolume( const Real_t x[8],
-   --x                        const Real_t y[8],
-   --x                        const Real_t z[8]);
-   type Eight_Reals is array (0..7) of Real_t;
-   function CalcElemVolume
-     (x : in Eight_Reals;
-      y : in Eight_Reals;
-      z : in Eight_Reals)
-     return Real_t;
-
+     (nodes : in NodesPerElement_Coordinate_Array)
+     return Volume_Type;
 
 end LULESH.Par;
